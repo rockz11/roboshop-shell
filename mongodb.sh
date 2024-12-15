5ed45c7e40ce2a32960bd071710d96460962d153
Prughvi <prudhviraj3525@gmail.com>
2024-12-15 19:36:43 +0530

    first commit in catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-org -y
+vim /etc/mongod.conf
 sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongodb.conf
 systemctl enable mongod
 systemctl start mongod
